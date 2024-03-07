4afb3b8b161401792c2636e748b152cdeb149d4e
Your Name <you@example.com>
2024-03-07 15:24:26 +0200

    Initional

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+dd(2);
 Route::get('/', function () {
     return view('welcome');
 });
