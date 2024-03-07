73794e745512274ef6de6a312c3277af04476faa
Your Name <you@example.com>
2024-03-07 18:20:34 +0200

    Initional

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', static function () {
+//    return view('welcome');
+    return redirect()->route('platform.main');
 });
 //Route::post('/admin/examples/filter/buttonClickTest', function (Request $request) {
 //    dd($request);
